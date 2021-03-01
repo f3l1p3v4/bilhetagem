7b926f092b73451aaec76cdc74f14a897a5cce84
f3l1p3v4 <fvaldez018@gmail.com>
2021-03-01 07:37:16 -0400

    created form

---
+++ src/components/car/carForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <Form>
+            olá
+        </Form>
+    </div> 
+</template>
+
+<script>
+import Form from '../base/Form';
+
+export default {
+    name: 'CarForm',
+    props: {
+        formModel: {
+            type: Object,
+            required: true
+        }
+    },
+    data () {
+        return {
+            innerFormModel: null
+        }
+    },
+    components: {
+        Form
+    }
+}
+</script>
---
+++ src/pages/CarFormPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page padding>
+    <CarForm :form-model="formModel" />
+  </q-page>
+</template>
+
+<script>
+import CarForm from '../components/car/CarForm.vue'
+export default {
+  name: 'CarFormPage',
+  props: {
+    formModel: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    CarForm
+  }
+}
+</script>
