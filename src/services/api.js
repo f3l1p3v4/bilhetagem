06b812295848925f35fbb61e5d461bb6b60d4513
Felipe Valdez <47610639+f3l1p3v4@users.noreply.github.com>
2022-12-05 08:42:10 -0400

    changed api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: "https://backend-bilhetagem.herokuapp.com",
+  baseURL: "https://ekv8js.sse.codesandbox.io/",
 })
 
 export default api;
