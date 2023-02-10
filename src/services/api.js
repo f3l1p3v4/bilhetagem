6ee80cc9e8616c3da1ac7e62268fcacb4714cd61
Felipe Valdez <47610639+f3l1p3v4@users.noreply.github.com>
2023-02-10 11:14:13 -0400

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: "https://ekv8js.sse.codesandbox.io/",
+  baseURL: "https://back-bilhetagem.onrender.com/",
 })
 
 export default api;
